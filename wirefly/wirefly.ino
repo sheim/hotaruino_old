90ca6f5fa1dfc3cac096c0b071672daf02da2ab9
sheim <heim.steve@gmail.com>
2015-07-28 10:57:40 +0900

    Add potentiometer to adjust threshold

--- wirefly/wirefly.ino
+++ wirefly/wirefly.ino
@@ -6,6 +6,9 @@ const int IR_SENSOR_PIN = A0;
 const int GREEN_LED_PIN = 10;
 const int IR_LED_PIN    = 9;
 const int RECEIVER_LED_PIN = 13;
+const int POT_THRESH_PIN = A1;
+int POT_THRESH_ZERO = 0; // though not technically constants because we define them in startup, should be thought of constants.
+int THRESH_ZERO = 0;
 
 // NOTE: Arduino UNO (and most others) treat doubles as floats.
 const double PI_HALF = 1.5; // chopped off just short of accurate, to avoid reaching a point where function is no longer concave-down.
@@ -19,7 +22,8 @@ double phase = 0;
 double omega = 0.01; // need to tune this, depends on loop speed
 double eps   = 0.05;
 double A = 1.2; //excitation level
-double threshold = 0;
+
+int threshold = 0; // threshold is read from analog, so int
 int iteration_counter = 1;
 
 // Timing
@@ -39,18 +43,29 @@ void setup() {
 
   analogWrite(IR_LED_PIN, 0); // make sure things are off
   delay(10);
+  double thresh = 0;
   for (int i = 1; i < 10; i++) { // get threshold value
-    threshold = threshold + analogRead(IR_SENSOR_PIN);
+    thresh = thresh + analogRead(IR_SENSOR_PIN);
     delay(50);
   }
-  threshold = threshold / 10; // Sample ambient light 100 times and average out to find a decent threshold.
-  threshold = threshold - 50; // TODO: add some safeguard, in case 50 is too much (i.e. room is too bright)
+  thresh = thresh / 10; // Sample ambient light 100 times and average out to find a decent thresh.
+  if(thresh > 50) {
+    thresh = thresh - 50;
+  }
+  THRESH_ZERO = thresh;
+  POT_THRESH_ZERO = analogRead(POT_THRESH_PIN);
+  threshold = THRESH_ZERO + analogRead(POT_THRESH_PIN) - POT_THRESH_ZERO;
   Serial.print("Threshold set at: ");
   Serial.println(threshold);
+
+  delay(1000); // delay a second, so start-up values can be read from the console
 }
 
 // the loop function runs over and over again forever
 void loop() {
+
+  // Adjust threshold
+  threshold = THRESH_ZERO + analogRead(POT_THRESH_PIN) - POT_THRESH_ZERO; // Could check first if potentiometer has moved, but I'm guessing it's not really faster
 
   // Check timing
   unsigned long current_millis = millis();
