c97ec33d4b85d316327fdb19df0d96e98da2501a
sheim <heim.steve@gmail.com>
2015-07-28 14:09:04 +0900

    Add potentiometer for coupling weight.

--- wirefly/wirefly.ino
+++ wirefly/wirefly.ino
@@ -3,12 +3,19 @@
 */
 
 const int IR_SENSOR_PIN = A0;
-const int GREEN_LED_PIN = 10;
+const int GREEN_LED_PIN = 8;
 const int IR_LED_PIN    = 9;
 const int RECEIVER_LED_PIN = 13;
 const int POT_THRESH_PIN = A1;
 int POT_THRESH_ZERO = 0; // though not technically constants because we define them in startup, should be thought of constants.
 int THRESH_ZERO = 0;
+
+// Coupling
+const int POT_COUPL_PIN = A2;
+double POT_COUPL_LOW = 0;
+double POT_COUPL_HIGH = 0;
+const double COUPL_UPPER = 3;
+const double COUPL_LOWER = 0;
 
 // NOTE: Arduino UNO (and most others) treat doubles as floats.
 const double PI_HALF = 1.5; // chopped off just short of accurate, to avoid reaching a point where function is no longer concave-down.
@@ -21,7 +28,7 @@ double X_RESET = 0.997; // chopped off just short of f(PI_HALF)
 double phase = 0;
 double omega = 0.01; // need to tune this, depends on loop speed
 double eps   = 0.05;
-double A = 1.2; //excitation level
+double coupling = 1.2; //excitation level
 
 int threshold = 0; // threshold is read from analog, so int
 int iteration_counter = 1;
@@ -40,7 +47,8 @@ void setup() {
   Serial.begin(9600);
   
   pinMode(RECEIVER_LED_PIN, OUTPUT);
-
+  pinMode(GREEN_LED_PIN, OUTPUT);
+  
   analogWrite(IR_LED_PIN, 0); // make sure things are off
   delay(10);
   double thresh = 0;
@@ -58,7 +66,30 @@ void setup() {
   Serial.print("Threshold set at: ");
   Serial.println(threshold);
 
-  delay(1000); // delay a second, so start-up values can be read from the console
+  // Coupling set up
+
+  Serial.println("Set potentiometer to min.");
+  digitalWrite(GREEN_LED_PIN,HIGH);
+  delay(3000);
+  POT_COUPL_LOW = analogRead(POT_COUPL_PIN);
+  Serial.println("Set potentiometer to max.");
+  digitalWrite(GREEN_LED_PIN,LOW);
+  delay(3000);
+  POT_COUPL_HIGH = analogRead(POT_COUPL_PIN);
+   digitalWrite(GREEN_LED_PIN,HIGH);
+  delay(100);
+  digitalWrite(GREEN_LED_PIN,LOW);
+  delay(100);
+  digitalWrite(GREEN_LED_PIN,HIGH);
+  delay(100);
+  digitalWrite(GREEN_LED_PIN,LOW);
+  delay(100);
+  digitalWrite(GREEN_LED_PIN,HIGH);
+  delay(100);
+  digitalWrite(GREEN_LED_PIN,LOW);
+
+  Serial.println(" Set up finished.");
+  delay(2000); // delay a second, so start-up values can be read from the console
 }
 
 // the loop function runs over and over again forever
@@ -71,15 +102,15 @@ void loop() {
   unsigned long current_millis = millis();
   // Toggle flash
   if (current_millis < (flash_start + flash_time)) { // flash
-    analogWrite(GREEN_LED_PIN, 255);
+    digitalWrite(GREEN_LED_PIN, HIGH);
     analogWrite(IR_LED_PIN, 255);
   }
   else {
-    analogWrite(GREEN_LED_PIN, 0); // turn off
+    digitalWrite(GREEN_LED_PIN, LOW); // turn off
     analogWrite(IR_LED_PIN, 0);
   }
 
-  // Check if sensors catches data
+  // Check if sensor catches data
   sensor_value = analogRead(IR_SENSOR_PIN);
   if (sensor_value < threshold) {
     flash_received = true;       // NOTE: could make this incrementable, so it the number of flashes seen is also important.
@@ -89,10 +120,15 @@ void loop() {
     digitalWrite(RECEIVER_LED_PIN, LOW);
   }
   // increment dynamics
+
+  // Check coupling weight here, so that each time-step delay is roughly the same
+  double pot_reading = analogRead(POT_COUPL_PIN);
+  coupling = map_double(pot_reading,0,1023,COUPL_LOWER,COUPL_UPPER);
+
   if (current_millis - previous_millis >= TIME_STEP) { // new timestep
     previous_millis = current_millis; // reset time
     if (flash_received) { // switch between active dynamics and passive
-      phase = phase + omega + A * eps; // Increment phase by eps.
+      phase = phase + omega + coupling * eps; // Increment phase by eps.
     }
     else {
       phase = phase + omega;
@@ -109,7 +145,14 @@ void loop() {
 
   // just some visiblity stuff
   iteration_counter = iteration_counter + 1;
-  if (iteration_counter % 12 == 0) {
-    Serial.println(sensor_value);
+  if (iteration_counter % 500 == 0) {
+    Serial.println(coupling);
   }
+}
+
+double map_double(double x, double in_min, double in_max, double out_min, double out_max)
+{
+ double temp = (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+ // temp = (int) (4*temp + .5);
+ return temp;
 }
